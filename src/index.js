0eee6a96819700fc9058e6076e1b7473e1fa049e
khargr14 <kay_hargrove@yahoo.com>
2021-04-15 23:23:40 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -14,3 +14,19 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import manageAuthorsAndBooks from './reducers/manageAuthorsAndBooks';
+
+
+const store = createStore(manageAuthorsAndBooks, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App store={store}/>
+  </Provider>,
+  document.getElementById('root')
+);
